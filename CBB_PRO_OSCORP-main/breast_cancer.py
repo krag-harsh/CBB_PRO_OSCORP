0a3e10207b50e4b6f99feb1aa3a941e114639fe8
Harsh Kumar Agarwal <harsh19423@iiitd.ac.in>
2020-12-20 20:47:33 +0530

    final files
    completed this project, front end and backend both the work is done

--- CBB_PRO_OSCORP-main/breast_cancer.py
+++ CBB_PRO_OSCORP-main/breast_cancer.py
@@ -1,5 +1,6 @@
 import pickle
 import os
+import json
 from flask import Flask,request,render_template,redirect,url_for,session,jsonify,make_response
 
 
@@ -7,27 +8,46 @@ from flask import Flask,request,render_template,redirect,url_for,session,jsonify
 breast_cancer_detector_model = pickle.load(open('breast_cancer_detector.pickle', 'rb'))
 # y_pred = breast_cancer_detector_model.predict(X_test_mean)
 # print(y_pred)
-
-app = Flask(__name__)
-app.config["SECRET_KEY"]="mysecretkey"
-@app.route('/')
-def index():
-    return render_template("index.html")
-@app.route('/process',methods=['POST'])
-def process():
-    radius=request.form['radius']
-    texture=request.form['texture']
-    perimeter=request.form['perimeter']
-    area = request.form['area']
-    concave= request.form['concave']
-    print("DG")
-    if(radius and texture and perimeter and area and concave):
-        y_pred = breast_cancer_detector_model.predict([[radius,texture,perimeter,area,concave]])
-        print(y_pred)
-        return jsonify({'y_pred':y_pred})
-    print("HG")
-    return jsonify({'error':'missing data'})
+if __name__ == '__main__':
+	app = Flask(__name__)
+	app.config["SECRET_KEY"]="mysecretkey"
+	@app.route('/hello', methods=['GET','POST'])
+	def hello():
+		print ("AFGS")
+		if request.method == 'POST':
+			print ("Incoming")
+			data = request.get_json ()
+			data = json.loads(data)
+			y_pred = breast_cancer_detector_model.predict ([[data["radius"], data["texture"], data["perimeter"], data["area"], data["concave"]]])
+			print(y_pred[0])
+			return str(y_pred[0]), 200
+		else:
+			print('alvndap')
+	
+	@app.route('/')
+	def cancer():
+		return render_template ("cancer.html")
+		
+	@app.route('/index',methods=['POST','GET'])
+	def index():
+		return render_template("index.html")
+	
+	@app.route('/process',methods=['POST'])
+	def process():
+		print("called")
+		radius=request.form['radius']
+		texture=request.form['texture']
+		perimeter=request.form['perimeter']
+		area = request.form['area']
+		concave= request.form['concave']
+		print("DG")
+		if(radius and texture and perimeter and area and concave):
+			y_pred = breast_cancer_detector_model.predict([[radius,texture,perimeter,area,concave]])
+			print(y_pred)
+			return jsonify({'y_pred': y_pred})
+		print("HG")
+		return jsonify({'error':'missing data'})
 
 if __name__=='__main__':
-    app.run(debug=True)
+	app.run(debug=True)
 
--- CBB_PRO_OSCORP-main/static/app.js
+++ CBB_PRO_OSCORP-main/static/app.js
@@ -1,5 +1,6 @@
 function mainApp() {
   const arrows = document.querySelectorAll(".fa-arrow-right");
+  const resultDiv = document.querySelector(".label-text .result");
   var inputsUser = new Array();
   var counter = 0;
   var json;
@@ -15,9 +16,8 @@ function mainApp() {
         console.log("Okay");
         inputsUser[counter++] = input.value;
 
-        if (nextForm.classList.contains("result")) {
-          convertToJSON(inputsUser);
-
+        if (nextForm.classList.contains("field-name6")) {
+          convertToJSON(inputsUser, nextForm);
         }
 
         next_form(parent, nextForm);
@@ -34,24 +34,39 @@ function mainApp() {
   });
 }
 
-function convertToJSON(inputsUser) {
-  json = JSON.parse(
+function convertToJSON(inputsUser, nextForm) {
+  json = JSON.stringify(
     `{"radius":${inputsUser[0]}, "texture":${inputsUser[1]}, "perimeter":${inputsUser[2]}, "area":${inputsUser[3]}, "concave":${inputsUser[4]}}`
   );
-  $.ajax({
-    json,
-    type:'/POST',
-    url: '/process'
+  console.log(json);
+  fetch("/hello", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: json
   })
-  .done(function(data){
-    if(data.error){
-      console.log("1");
-    }
-    else{
-      console.log("0");
-    }
-  });
-  console.log(JSON.stringify(json));
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(text) {
+      console.log("POST response : ");
+      console.log(text);
+      if (text == 1.0) {
+        console.log("abcd");
+        nextForm.innerHTML += `<h3 class="label-text result" style="left: 15%" ;>
+        Malignant Cancer
+        <span class="tooltip" style="top: -120px" ;>Malignant tumor, also known as cancerous cells, on the other hand, have the property of metastasis i.e. they spread rapidly to other parts of the body. They can occur anywhere in the body and can even be carried by the blood or lymphatic system. They often require intense treatment such as surgery, radiation, chemotherapy and immunotherapy. 
+They may recur after removal, sometimes in areas other the original site</span>
+      </h3>`;
+      } else if (text == 0.0) {
+        nextForm.innerHTML = `<h3 class="label-text result" style="left: 15%" ;>
+        Benign Cancer
+        <span class="tooltip" style="top: -120px" ;>Benign tumor also known as non cancerous cells don't spread to other areas of the body, a property known as metastasis. Thus, these tumors are less worrisome unless they pressing on nearby tissues, nerves, blood vessels or on the vital organs blocking channels. They can be removed by surgery and are unlikely to reoccur after removal.
+</span>
+      </h3>`;
+      }
+    });
 }
 function next_form(parent, nextForm) {
   parent.classList.add("innactive");
